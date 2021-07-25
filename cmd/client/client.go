6602b3fc3fc2571e8a8d302c4efca8fe985fdb20
Charles Willian <wcharlesw15@gmail.com>
2021-07-25 15:36:04 -0300

    feat: bidirectional communication between client and server

--- cmd/client/client.go
+++ cmd/client/client.go
@@ -24,7 +24,8 @@ func main() {
 	client := pb.NewUserServiceClient(connection)
 	// AddUser(client)
 	// AddUserVerbose(client)
-	AddUsers(client)
+	// AddUsers(client)
+	AddUsersStreamBoth(client)
 }
 
 func AddUser(client pb.UserServiceClient) {
@@ -120,4 +121,75 @@ func AddUsers(client pb.UserServiceClient) {
 	}
 
 	fmt.Println(res)
+}
+
+func AddUsersStreamBoth(client pb.UserServiceClient) {
+	stream, err := client.AddUserStreamBoth(context.Background())
+
+	if err != nil {
+		log.Fatalf("Error creating request: %v", err)
+	}
+
+	reqs := []*pb.User{
+		&pb.User{
+			Id:    "0",
+			Name:  "Jao",
+			Email: "jao0@gmail.com",
+		},
+		&pb.User{
+			Id:    "1",
+			Name:  "Jao1",
+			Email: "jao1@gmail.com",
+		},
+		&pb.User{
+			Id:    "02",
+			Name:  "Jao2",
+			Email: "jao02@gmail.com",
+		},
+		&pb.User{
+			Id:    "03",
+			Name:  "Jao3",
+			Email: "jao03@gmail.com",
+		},
+		&pb.User{
+			Id:    "04",
+			Name:  "Jao5",
+			Email: "jao05@gmail.com",
+		},
+	}
+
+	// communicate 2 or more go routines
+	// here we are using to block operations
+	wait := make(chan int)
+
+	go func() {
+		for _, req := range reqs {
+			fmt.Println("Sending user: ", req.Name)
+			stream.Send(req)
+			time.Sleep(time.Second * 2)
+		}
+		stream.CloseSend()
+	}()
+
+	go func() {
+		for {
+			res, err := stream.Recv()
+			if err == io.EOF {
+				break
+			}
+			if err != nil {
+				log.Fatalf("Error receiving data: %v", err)
+				break
+			}
+			fmt.Printf(
+				"Receiving user %v, com status: %v\n",
+				res.GetUser().GetName(),
+				res.GetStatus(),
+			)
+		}
+		close(wait)
+	}()
+
+	// while channel exists the app will still run
+	<-wait
 }
